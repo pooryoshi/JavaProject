ee7681515cec3a48989347a231629a95c979dd95
pooryoshi <1178098368@qq.com>
2019-10-05 16:08:57 +0800

    time_exp(1)

--- time_exp/src/time_exp/time_1.java
+++ time_exp/src/time_exp/time_1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class time_1 {
  	public static void main(String[] args) {
  		Date d1 = new Date();
- 		System.out.println("中国");
+ 		System.out.println("时间");
  		System.out.println(d1);
  		System.out.println();
  	}
