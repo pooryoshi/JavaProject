c20f19cd9aa01fe53484cf806b4987f32ee4d670
pooryoshi <1178098368@qq.com>
2019-10-05 16:20:53 +0800

    time(2)

--- time_exp/src/time_exp/time_1.java
+++ time_exp/src/time_exp/time_1.java
@@ -3,11 +3,8 @@ import java.util.*;
 public class time_1 {
  	public static void main(String[] args) {
  		Date d1 = new Date();
- 		System.out.println("时间");
+ 		System.out.println("pctime");
  		System.out.println(d1);
  		System.out.println();
  	}
-	
-	
-	
 }
